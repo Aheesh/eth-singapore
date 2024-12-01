7bab7d8d9d8957b7c3538e956dae09fad250073c
aheesh <aheesh@gmail.com>
2024-12-01 08:40:54 +0530

    meta tags format updated in layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,15 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://eth-singapore-woad.vercel.app/api/generateImage" />
+        <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+        <meta property="fc:frame:button:1" content="Ding" />
+        <meta property="fc:frame:button:2" content="Gukesh" />
+        <meta property="fc:frame:button:3" content="Draw" />
+        <meta property="fc:frame:post_url" content="https://eth-singapore-woad.vercel.app/api/frame" />
+      </head>
       <body>{children}</body>
     </html>
   );
